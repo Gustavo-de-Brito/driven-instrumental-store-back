1eb03603ab7202b1916676192fac16e4db6e560b
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-07-08 10:54:01 -0300

    adiciona get na rota /products

---
+++ src/controllers/productsControllers.js
@@ -0,0 +1,11 @@
+import { db } from "../databases/mongodb.js";
+
+export async function getProducts(req, res) {
+  try {
+    const products = await db.collection("products").find().toArray();
+  
+    res.status(200).send(products);
+  } catch(err) {
+    res.sendStatus(500);
+  }
+}
